b342b4120e475a122d2179b6b2e92a857f734aaa
Jun Lee <Lee.Jun@team.telstra.com>
2014-08-19 13:44:34 +0800

    add rocket

--- SpirteWalkthrough/SpaceShipScene.m
+++ SpirteWalkthrough/SpaceShipScene.m
@@ -8,6 +8,14 @@
 
 #import "SpaceShipScene.h"
 
+static inline CGFloat skRandf() {
+    return rand() / (CGFloat) RAND_MAX;
+}
+
+static inline CGFloat skRand(CGFloat low, CGFloat high) {
+    return skRandf() * (high - low) + low;
+}
+
 @interface SpaceShipScene()
 @property BOOL contentCreated;
 @end
@@ -26,6 +34,65 @@
 {
     self.backgroundColor = [SKColor blackColor];
     self.scaleMode = SKSceneScaleModeAspectFit;
+    //spaceship
+    SKSpriteNode *spaceship = [self newSpaceship];
+    spaceship.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
+    //rock
+    SKAction * makeRocks = [SKAction sequence:@[[SKAction performSelector:@selector(addRock) onTarget:self],[SKAction waitForDuration:0.10 withRange:0.15]]];
+    [self runAction:[SKAction repeatActionForever:makeRocks]];
+    [self addChild:spaceship];
+}
+
+-(SKSpriteNode *)newSpaceship{
+//    SKSpriteNode * hull = [[SKSpriteNode alloc] initWithColor:[SKColor grayColor] size:CGSizeMake(64, 32)];
+    SKTexture *texture=[SKTexture textureWithImageNamed:@"rocket"];
+    SKSpriteNode *hull = [SKSpriteNode spriteNodeWithTexture:texture];
+    hull.position = CGPointMake(100,100);
+    hull.anchorPoint = CGPointMake(0.5,0.5);
+    hull.color = [SKColor redColor];
+    hull.colorBlendFactor = 0.5;
+    hull.blendMode = SKBlendModeAdd;
+    hull.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:hull.size];
+    hull.physicsBody.dynamic = NO;
+    SKAction * hover = [SKAction sequence:@[[SKAction waitForDuration:1.0],
+                                            [SKAction moveByX:100 y:50.0 duration:1.0],
+                                            [SKAction waitForDuration:1.0],
+                                            [SKAction moveByX:-100.0 y:-50 duration:1.0]]];
+    [hull runAction:[SKAction repeatActionForever:hover]];
+    
+    SKSpriteNode *light1 = [self newLight];
+    light1.position = CGPointMake(-28.0, 6.0);
+    [hull addChild:light1];
+    SKSpriteNode *light2 = [self newLight];
+    light2.position = CGPointMake(28.0, 6.0);
+    [hull addChild:light2];
+    
+    return hull;
+}
+
+-(SKSpriteNode *) newLight{
+    SKSpriteNode *light = [[SKSpriteNode alloc] initWithColor:[SKColor yellowColor] size:CGSizeMake(8, 8)];
+    SKAction *blink = [SKAction sequence:@[[SKAction fadeOutWithDuration:0.25],[SKAction fadeInWithDuration:0.25]]];
+    SKAction *blinkForever = [SKAction repeatActionForever:blink];
+    [light runAction:blinkForever];
+    return light;
+}
+
+-(void)addRock{
+    SKSpriteNode *rock = [[SKSpriteNode alloc] initWithColor:[SKColor brownColor] size:CGSizeMake(8, 8)];
+    rock.position = CGPointMake(skRand(0, self.size.width),self.size.height-50);
+    rock.name = @"rock";
+    rock.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:rock.size];
+    rock.physicsBody.usesPreciseCollisionDetection = YES;
+    [self addChild:rock];
+}
+
+-(void)didSimulatePhysics
+{
+    [self enumerateChildNodesWithName:@"rock" usingBlock:^(SKNode *node, BOOL *stop) {
+        if (node.position.y < 0)
+            [node removeFromParent];
+    }];
 }
 
 @end
